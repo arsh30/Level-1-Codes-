fbb3dd14fd6eb8f227b1d5a661ba236601908eca
Arsh <as3096734@gmail.com>
2021-11-19 22:30:56 -0800

    22 dec upto Functions class 1 Finish

--- l001_Basics/l001Basics.java
+++ l001_Basics/l001Basics.java
@@ -31,11 +31,39 @@ public class l001Basics {
         int a = scn.nextInt(); //in java:if we take the input first we import the scanner and make public static scanner scn = new scanner(system,in) then make variables in main
         int b = scn.nextInt();
         
-        System.out.println(multiply(a, b));
-        System.out.println(add(a, b));
-        System.out.println(subtract(a, b));
-        System.out.println(divide(a, b));
+        // System.out.println(multiply(a, b));
+        // System.out.println(add(a, b));
+        // System.out.println(subtract(a, b));
+        // System.out.println(divide(a, b));
+
+        printZ();
+        int marks = scn.nextInt();
+        gradingSystem(marks);
     }
     //START WITH FUNCTIONS
+
+    //ques1 printz
+    public static void printZ() {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+
+    //ques2 -> Grading System
+    public static void gradingSystem(int marks) {
+        if (marks > 90) {
+            System.out.println("excellent");
+        } else if (marks > 80 && marks <= 90) {
+            System.out.println("good");
+        } else if (marks > 70 && marks <= 80) {
+            System.out.println("fair");
+        } else if (marks > 60 && marks <= 70) {
+            System.out.println("meets expectations");
+        } else {
+            System.out.println("below par");
+        }
+    }
 
 }
