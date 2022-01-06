e0c01b374ac0c70cf631bf8f585c921662a51684
Arsh <as3096734@gmail.com>
2022-01-06 06:01:19 -0800

    upto 17th jan completed recursion

--- l006_Recursion/l002.java
+++ l006_Recursion/l002.java
@@ -95,18 +95,18 @@ STRINGBUILDER -> in this 1) we add , 2) call lgai, 3)  then jo add kra hai toh u
     return myAns;
   }
 
-  static String[] codes = {
-    ".;",
-    "abc",
-    "def",
-    "ghi",
-    "jkl",
-    "mno",
-    "pqrs",
-    "tu",
-    "vwx",
-    "yz",
-  }; //make this: to check which character does he have and to access those
+  // static String[] codes = {
+  //   ".;",
+  //   "abc",
+  //   "def",
+  //   "ghi",
+  //   "jkl",
+  //   "mno",
+  //   "pqrs",
+  //   "tu",
+  //   "vwx",
+  //   "yz",
+  // }; //make this: to check which character does he have and to access those
 
   public static int printKPC(String str, int idx, String ans) {
     if (idx == str.length()) {
@@ -159,12 +159,191 @@ STRINGBUILDER -> in this 1) we add , 2) call lgai, 3)  then jo add kra hai toh u
     return myAns;
   }
 
+  //17th Jan,2021
+
+  // sr - source row
+  // sc - source column
+  // dr - destination row
+  // dc - destination column
+  public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) { //2call horizontal and vertical
+    if (sr == dr && sc == dc) {
+      ArrayList<String> base = new ArrayList<>();
+      base.add("");
+      return base;
+    }
+
+    ArrayList<String> myAns = new ArrayList<>();
+
+    // 1st check krke move krege ki valid hai aage jana
+    if (sc + 1 <= dc) {
+      ArrayList<String> Horizontal = getMazePaths(sr, sc + 1, dr, dc);
+      // now we have to add 'h' jo bhi al me aaya
+      for (String s : Horizontal) {
+        myAns.add("h" + s);
+      }
+    }
+
+    if (sr + 1 <= dr) {
+      ArrayList<String> Vertical = getMazePaths(sr + 1, sc, dr, dc);
+      for (String s : Vertical) {
+        myAns.add("v" + s);
+      }
+    }
+
+    return myAns;
+  }
+
+  public static ArrayList<String> getMazePaths_01(
+    int sr,
+    int sc,
+    int dr,
+    int dc
+  ) { //2call horizontal and vertical
+    if (sr == dr && sc == dc) {
+      ArrayList<String> base = new ArrayList<>();
+      base.add("");
+      return base;
+    }
+
+    ArrayList<String> myAns = new ArrayList<>();
+
+    // 1st check krke move krege ki valid hai aage jana
+    if (sc + 1 <= dc) {
+      ArrayList<String> Horizontal = getMazePaths_01(sr, sc + 1, dr, dc);
+      // now we have to add 'h' jo bhi al me aaya
+      for (String s : Horizontal) {
+        myAns.add("h" + s);
+      }
+    }
+
+    if (sc + 1 <= dc && sr + 1 <= dr) {
+      ArrayList<String> Diagonal = getMazePaths_01(sr + 1, sc + 1, dr, dc);
+      // now we have to add 'h' jo bhi al me aaya
+      for (String s : Diagonal) {
+        myAns.add("d" + s);
+      }
+    }
+
+    if (sr + 1 <= dr) {
+      ArrayList<String> Vertical = getMazePaths_01(sr + 1, sc, dr, dc);
+      for (String s : Vertical) {
+        myAns.add("v" + s);
+      }
+    }
+
+    return myAns;
+  }
+
+  public static int printMazePaths(int sr, int sc, int dr, int dc, String ans) {
+    if (sr == dr && sc == dc) {
+      System.out.println(ans);
+      return 1; //return 1 because this is mostly used in dp means hmne kitne baar base case hit kra or kitni baar answer bna hmara
+    }
+    int count = 0;
+    //horizontal
+    if (sc + 1 <= dc) count += printMazePaths(sr, sc + 1, dr, dc, ans + 'h');
+    if (sr + 1 <= dr) count += printMazePaths(sr + 1, sc, dr, dc, ans + 'v');
+
+    return count;
+    /*
+    Faith of wayup ->
+    1) jb way ki call lgate hai toh jo kam krna hota hai usi time krlete hai ik hi call me
+
+    2)jo cheez hmne follow kri hai, jo kam hm krre hai vohi same work faith (idx + 1) wala bhi krke laega
+    */
+
+  }
+
+  public static void printMazePaths_01(
+    int sr,
+    int sc,
+    int dr,
+    int dc,
+    String ans
+  ) {
+    if (sr == dr && sc == dc) {
+      System.out.println(ans);
+      return;
+    }
+
+    // horizontal
+    for (int jump = 1; sc + jump <= dc; jump++) printMazePaths_01(
+      sr,
+      sc + jump,
+      dr,
+      dc,
+      ans + "h" + jump
+    );
+
+    // vertical
+    for (int jump = 1; sr + jump <= dr; jump++) printMazePaths_01(
+      sr + jump,
+      sc,
+      dr,
+      dc,
+      ans + "v" + jump
+    );
+
+    // diagonal
+    for (
+      int jump = 1;
+      sc + jump <= dc && sr + jump <= dr;
+      jump++
+    ) printMazePaths_01(sr + jump, sc + jump, dr, dc, ans + "d" + jump);
+  }
+
+  public static ArrayList<String> getMazePaths_multi(
+    int sr,
+    int sc,
+    int dr,
+    int dc
+  ) {
+    if (sr == dr && sc == dc) {
+      ArrayList<String> base = new ArrayList<>();
+      base.add("");
+      return base;
+    }
+    ArrayList<String> myAns = new ArrayList<>();
+
+    for (int jump = 1; sc + jump <= dc; jump++) {
+      ArrayList<String> Horizontal = getMazePaths_multi(sr, sc + jump, dr, dc);
+      for (String s : Horizontal) {
+        myAns.add("h" + jump + s);
+      }
+    }
+    for (int jump = 1; sr + jump <= dr; jump++) {
+      ArrayList<String> Vertical = getMazePaths_multi(sr + jump, sc, dr, dc);
+      for (String s : Vertical) {
+        myAns.add("v" + jump + s);
+      }
+    }
+    for (int jump = 1; sc + jump <= dc && sr + jump <= dr; jump++) {
+      ArrayList<String> Diagonal = getMazePaths_multi(
+        sr + jump,
+        sc + jump,
+        dr,
+        dc
+      );
+      for (String s : Diagonal) {
+        myAns.add("d" + jump + s);
+      }
+    }
+
+    return myAns;
+  }
+
   public static void main(String[] args) {
     // String str = scn.next();
     // printSS(str, 0, "");
-    String str = scn.nextLine();
-    ArrayList<String> ans = printKPC_ret(str, 0); //note: jo data hm return type se krre hai final answer usi me catch krege
-    System.out.println(ans);
+    // String str = scn.nextLine();
+    // ArrayList<String> ans = printKPC_ret(str, 0); //note: jo data hm return type se krre hai final answer usi me catch krege
+    // System.out.println(ans);
     // subsequence();
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+
+    ArrayList<String> ans = getMazePaths(0, 0, n - 1, m - 1);
+    // jo niche ques me return type me answer bnaya usi me catch krege
+    System.out.println(ans);
   }
 }
