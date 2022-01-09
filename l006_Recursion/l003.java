feb2946be8a200dc8ddc36c5c0b9f5b3a24c9458
Arsh <as3096734@gmail.com>
2022-01-09 04:09:13 -0800

    recursion complete

--- l006_Recursion/l003.java
+++ l006_Recursion/l003.java
@@ -43,13 +43,22 @@ public class l003 {
     return count;
   }
 
-//   Radius concept -> means agar hmko print krna ho circle me to aise krege ki ik radius ka loop lgadenege pehle at max 1 ,1 ki hi jump 
-// lekr jara hai, radius concept concept is that max jump leskta hai then accoriding to radius
+  //   Radius concept -> means agar hmko print krna ho circle me to aise krege ki ik radius ka loop lgadenege pehle at max 1 ,1 ki hi jump
+  // lekr jara hai, radius concept concept is that max jump leskta hai then accoriding to radius
 
-public static int floodFill_withJump(int sr,int sc, int er, int ec, boolean[][] vis, int[][] dir,String[] dirS, String ans) {
-    if(sr == er && sc == ec) {
-        System.out.println(ans);
-        return 1;
+  public static int floodFill_withJump(
+    int sr,
+    int sc,
+    int er,
+    int ec,
+    boolean[][] vis,
+    int[][] dir,
+    String[] dirS,
+    String ans
+  ) {
+    if (sr == er && sc == ec) {
+      System.out.println(ans);
+      return 1;
     }
     // use -> radius loop
     vis[sr][sc] = true;
@@ -58,33 +67,105 @@ public static int floodFill_withJump(int sr,int sc, int er, int ec, boolean[][] 
     int m = vis[0].length;
     int count = 0;
 
-    for(int rad = 1; rad <= Math.max(n,m); rad++)
-    for(int d = 0; d < dir.length; d++) {
-        int r = sr + rad * dir[d][0];
-        int c = sc + rad * dir[d][1]; //unit vector j cap
+    for (int rad = 1; rad <= Math.max(n, m); rad++) for (
+      int d = 0;
+      d < dir.length;
+      d++
+    ) {
+      int r = sr + rad * dir[d][0];
+      int c = sc + rad * dir[d][1]; //unit vector j cap
 
-        // check that we are in range in or not
-        if(r >= 0 && c >= 0 && r < n && c < m && !vis[r][c]){
-            count += floodFill_withJump(r,c,er,ec,vis,dir,dirS, ans + dirS[d] + rad);
-        }
+      // check that we are in range in or not
+      if (r >= 0 && c >= 0 && r < n && c < m && !vis[r][c]) {
+        count +=
+          floodFill_withJump(r, c, er, ec, vis, dir, dirS, ans + dirS[d] + rad);
+      }
     }
-    
-    vis[sr][sc] = false;  //this is backtracking
-    return count;
 
-}
-    // https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
-    // https://www.geeksforgeeks.org/count-number-ways-reach-destination-maze/
-    // https://www.geeksforgeeks.org/rat-in-a-maze-with-multiple-steps-jump-allowed/?ref=rp
-    // https://www.geeksforgeeks.org/rat-in-a-maze-problem-when-movement-in-all-possible-directions-is-allowed/?ref=rp
+    vis[sr][sc] = false; //this is backtracking
+    return count;
+  }
+
+  public static void displayBoard(int[][] chess) {
+    for (int i = 0; i < chess.length; i++) {
+      for (int j = 0; j < chess[0].length; j++) {
+        System.out.print(chess[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    System.out.println();
+  }
+
+  public static int knightTour(
+    int sr,
+    int sc,
+    int move,
+    int tnc,
+    int[][] vis,
+    int[][] dir
+  ) {
+    if (move == tnc) {
+      // hmne matrix 0 se start kra hai to 25 tnc me 24 call lg kr add hoga ik bchega usko add krege
+      vis[sr][sc] = move;
+      displayBoard(vis);
+      vis[sr][sc] = 0;
+      return 1;
+    }
+
+    vis[sr][sc] = move; //initial move aate hi
+
+    int n = vis.length;
+    int m = vis[0].length;
+    int count = 0;
+    for (int d = 0; d < dir.length; d++) {
+      int r = sr + dir[d][0];
+      int c = sc + dir[d][1];
+
+      // check we are in the boundary or not
+      if (r >= 0 && c >= 0 && r < n && c < m && vis[r][c] == 0) {
+        count += knightTour(r, c, move + 1, tnc, vis, dir);
+      }
+    }
+    vis[sr][sc] = 0;
+    return count;
+  }
+
+  public static int findTargetSumWays(int[] nums, int S, int idx) {
+    if (idx == nums.length) { 
+      return S == 0 ? 1 : 0;
+    }
+    int count = 0;
+
+    count += findTargetSumWays(nums, S - nums[idx], idx + 1); //S is target eg S = 100, 100 - nums[idx(10)] = 90 so idx + 1apna target le aao
+    count += findTargetSumWays(nums, S - (-nums[idx]), idx + 1);
+    return count;
+  }
+
+  // https://www.geeksforgeeks.org/rat-in-a-maze-backtracking-2/
+  // https://www.geeksforgeeks.org/count-number-ways-reach-destination-maze/
+  // https://www.geeksforgeeks.org/rat-in-a-maze-with-multiple-steps-jump-allowed/?ref=rp
+  // https://www.geeksforgeeks.org/rat-in-a-maze-problem-when-movement-in-all-possible-directions-is-allowed/?ref=rp
 
   public static void main(String[] args) {
-    int[][] dir = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+    // int[][] dir = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+    int[][] dir = {
+      { -2, 1 },
+      { -1, 2 },
+      { 1, 2 },
+      { 2, 1 },
+      { 2, -1 },
+      { 1, -2 },
+      { -1, -2 },
+      { -2, -1 },
+    };
     String[] dirS = { "L", "D", "R", "U" };
 
     int n = 3, m = 3;
     boolean[][] vis = new boolean[n][m];
     // System.out.println(floodFill(0, 0, n - 1, m - 1, vis, dir, dirS, ""));
-   System.out.println(floodFill_withJump(0,0,n-1,m-1,vis,dir,dirS,""));
-}
+    System.out.println(
+      floodFill_withJump(0, 0, n - 1, m - 1, vis, dir, dirS, "")
+    );
+  }
 }
---
+++ l007_TimeAndSpaceComplexity/l001.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class l001 {
+
+  public static Scanner scn = new Scanner(System.in);
+
+  public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void moveToLast(int arr[], int si, int ei) {
+    // sbse first element ko last me pahunch aayege
+    // so first write first iteration
+    for (int i = si + 1; i <= ei; i++) {
+      if (arr[i - 1] > arr[i]) swap(arr, i - 1, i);
+    }
+  }
+
+  public static void bubble(int arr[], int n) {
+    int ei = n - 1;
+    for (int i = 0; i < n; i++) { //n is size, iterations
+      moveToLast(arr, 0, ei - i);
+      //means 1st 0 to ei - 1 ie 8, 2nd 0 to ei - 2 ie 7
+      //means 3rd 0 to ei - 3 ie 7, 4h 0 to ei - 4 ie 6.....
+    }
+  }
+
+  //   SELECTION SORT
+  public static int moveToStart(int[] arr, int si, int ei) {
+    int smallIdx = si; //isme smallIdx lenge then sabse compare krege then return krege niche
+
+    for (int i = si + 1; i <= ei; i++) {
+      if (isSmaller(arr, i, smallIdx)) smallIdx = i;
+    }
+    return smallIdx;
+  }
+
+  public static void selectionSort(int[] arr) {
+    // here we send the starting value and ending values ie si,ei
+    int n = arr.length;
+    int ei = n - 1;
+    for (int i = 0; i < n - 1; i++) { //means jb last me 2 elem bchege to unko compare krne ka koi faida nahi
+      int idx = moveToStart(arr, i, ei);
+      swap(arr, i, idx);
+    }
+  }
+
+  public static void main(String[] args) {}
+}
