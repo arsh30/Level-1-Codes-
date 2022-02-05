72a9a81a2ec6c9e5ec18d564f3d579fd8cb70a12
Arsh <as3096734@gmail.com>
2022-02-05 02:21:48 -0800

    complete trees generic tree 18th march

--- l009_Trees/l003_GT.java
+++ l009_Trees/l003_GT.java
@@ -249,4 +249,39 @@ public class l003_GT {
     }
     return true;
   }
+
+  public static boolean areMirror(Node n1, Node n2) {
+    //check on the basis of size
+    if (n1.childs.size() != n2.childs.size()) {
+      return false;
+    }
+    int n = n1.childs.size();
+    for (int i = 0, j = n - 1; i < n; i++, j--) {
+      Node c1 = n1.childs.get(i);
+      Node c2 = n2.childs.get(j);
+
+      if (areMirror(c1, c2) == false) return false;
+    }
+    return true;
+  }
+
+  //are tree is symmetric -> means it is foldable (if their size is size is same means it is symmetric)
+  public static boolean areMirror(Node n1, Node n2) {
+    //check on the basis of size
+    if (n1.children.size() != n2.children.size()) {
+      return false;
+    }
+    int n = n1.children.size();
+    for (int i = 0, j = n - 1; i < n; i++, j--) {
+      Node c1 = n1.children.get(i);
+      Node c2 = n2.children.get(j);
+
+      if (areMirror(c1, c2) == false) return false;
+    }
+    return true;
+  }
+
+  public static boolean IsSymmetric(Node node) {
+    return areMirror(node, node);
+  }
 }
