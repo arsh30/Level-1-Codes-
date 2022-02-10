987631c8b9f408a06b2c3f4d371196a74bd733c5
Arsh <as3096734@gmail.com>
2022-02-09 17:23:17 -0800

    class 1st complete upto queue01

---
+++ l010_StackAndQues/Queue Using LinkedList/queue01.java
@@ -0,0 +1,63 @@
+//in this we dont use any linked list, we make our own linked list
+public class queue01 {
+
+  public class Node {
+
+    int data = 0;
+    Node nex = null;
+
+    //constructor
+    Node(int data) {
+      this.data = data;
+    }
+  }
+
+  private Node head = null;
+  private Node tail = null;
+  private int elementCount = 0;
+
+  private void addLastNode(int data) {
+    Node node = new Node(data);
+    if (this.elementCount == 0) { //means head tail null pr hai
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.elementCount++;
+  }
+
+  private int removeFirst() {
+    Node removeNode = this.head;
+    if (this.elementCount == 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removeNode.next;
+    }
+    removeNode.next = null;
+    this.elementCount--;
+    return removeNode;
+  }
+
+  public int size() {
+    return this.elementCount;
+  }
+
+  public boolean isEmpty() {
+    return this.elementCount == 0;
+  }
+
+  public void push(int data) {
+    addLastNode(data);
+  }
+
+  public int front() {
+    return this.head.data;
+  }
+
+  public int pop() {
+    return removeFirst();
+  }
+}
