cc9818a914a529080fd797b263f0fa101945e6b4
Arsh <as3096734@gmail.com>
2022-02-13 01:14:57 -0800

    ques2 complete class 2

--- l012_Heap/l001.java
+++ l012_Heap/l001.java
@@ -15,19 +15,49 @@ public class l001 {
     }
   }
 
-  public static void maxPQ(){
-      PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+  public static void maxPQ() {
+    PriorityQueue<Integer> pq = new PriorityQueue<>(
+      (a, b) -> {
         //   return a - b; //this - other gives default behaviour [in ascending order]
-            return b - a;  //other - this reverse of default behaviour
-      }); 
-      for(int i = 10; i >= 0; i--){
-          pq.add(i);
+        return b - a; //other - this reverse of default behaviour
       }
-      while(pq.size() != 0){
-          int ele = pq.remove();
-          System.out.print(ele + " ");
-      }
+    );
+    for (int i = 10; i >= 0; i--) {
+      pq.add(i);
+    }
+    while (pq.size() != 0) {
+      int ele = pq.remove();
+      System.out.print(ele + " ");
+    }
+  }
 
+  public static void kLargestElements(int[] arr, int k) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    for (int ele : arr) {
+      pq.add(ele);
+      if (pq.size() > k) {
+        pq.remove();
+      }
+    }
+    while (pq.size() != 0) {
+      System.out.println(pq.remove());
+    }
+  }
+
+  public static void kSmallest(int[] arr, int k) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>(
+      (a, b) -> {
+        return b - a;
+      }
+    );
+    for (int ele : arr) {
+      pq.add(ele);
+      if (pq.size() > k) pq.remove();
+    }
+
+    while (pq.size() != 0) {
+      System.out.println(pq.remove());
+    }
   }
 
   public static void main(String[] args) {
