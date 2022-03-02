c1916fe531777e83354687041754dc569e9d5f72
Arsh <as3096734@gmail.com>
2022-03-01 22:26:45 -0800

    class 2 completed

--- l013_Graphs/l001_Basic.java
+++ l013_Graphs/l001_Basic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class l001_Basic {
 
@@ -71,13 +72,163 @@ public class l001_Basic {
     }
   }
 
+  //class 2
+
   public static boolean hasPath(int src, int dest, boolean[] vis) {
-    if (src == dest) return false;
+    if (src == dest) {
+      return true;
+    }
+
     boolean res = false;
+    vis[src] = true;
     for (Edge e : graph[src]) { //sare nbr pr call lgayi
-      if (!vis[e.v]) res = res || hasPath(e.v, dest, vis);
+      if (!vis[e.v]) res = res || hasPath(e.v, dest, vis); // res = res || hasPath(e.v, dest, vis);
     }
     return res;
+  }
+
+  public static int allPaths(int src, int dest, boolean[] vis, String ans) { //same as has path, bss isme false mark krege jab jaege
+    if (src == dest) {
+      System.out.println(ans + dest);
+      return 1;
+    }
+    int count = 0;
+
+    vis[src] = true; //because infinite call na lgjaye
+
+    for (Edge e : graph[src]) { //ik edge me e or v hai to hme v compare krna
+      if (!vis[e.v]) count += allPaths(e.v, dest, vis, ans + src); //edge ke andr destination dekhna that is v
+    }
+
+    vis[src] = false;
+    return count;
+  }
+
+  public static void printPreOrder(
+    int src,
+    boolean[] vis,
+    String ans,
+    int wsf
+  ) {
+    //it does not any base case because jab sare true hojege to call lgegi hi nahi
+
+    System.out.println(src + " -> " + ans + src + " @ " + wsf);
+    vis[src] = true; //step 1
+    for (Edge e : graph[src]) { //step 2 -> visited nbr -> we get the (0,1) and (0,3) edge
+      if (!vis[e.v]) printPreOrder(e.v, vis, ans + src, wsf + e.w);
+    }
+    vis[src] = false;
+  }
+
+  public static void postOrder(int src, boolean[] vis, String ans, int wsf) {
+    vis[src] = true;
+    for (Edge e : graph[src]) {
+      if (!vis[e.v]) postOrder(e.v, vis, ans + src, wsf + e.w);
+    } //this for loop complete 1st iteration complete to print 1st postOrder
+    System.out.println(src + "->" + ans + src + "@" + wsf);
+    vis[src] = false;
+  }
+
+  public static class Pair {
+
+    int largestWeight = -(int) 1e9;
+    String largestPath = "";
+    int smallestWeight = (int) 1e9;
+    String smallestPath = "";
+
+    int ceil = (int) 1e9;
+    String ceilPath = "";
+    int floor = -(int) 1e9;
+    String floorPath = "";
+  }
+
+  // to find kth largest
+  public static class pqPair {
+
+    int wsf = 0;
+    String psf = ""; //it  is a answer
+
+    pqPair(int wsf, String psf) {
+      this.wsf = wsf;
+      this.psf = psf;
+    }
+  }
+
+  static PriorityQueue<pqPair> pq = new PriorityQueue<>(
+    (a, b) -> {
+      return a.wsf - b.wsf; //compare on terms of weight
+    }
+  );
+
+  public static void allSolution(
+    int src,
+    int dest,
+    boolean[] vis,
+    Pair p,
+    String psf,
+    int wsf,
+    int givenWeight,
+    int k
+  ) {
+    if (src == dest) {
+      //means sare answer idr hi milege, to jo work hoga idr hi hoga
+      if (wsf > p.largestWeight) {
+        p.largestWeight = wsf;
+        p.largestPath = psf + dest;
+      }
+      if (wsf < p.smallestWeight) {
+        p.smallestWeight = wsf;
+        p.smallestPath = psf + dest;
+      }
+
+      //Now find ceil and floor
+      if (wsf < givenWeight) {
+        p.floor = Math.max(p.floor, wsf); //idr sare weight to aagye hai tbhi
+      }
+      if (wsf > givenWeight) {
+        p.ceil = Math.min(p.ceil, wsf);
+      }
+
+      //find kth largest
+      pq.add(new pqPair(wsf, psf + dest)); //add wsf and psf
+      if (pq.size() > k) {
+        pq.remove();
+      }
+
+      return;
+    }
+    vis[src] = true;
+    for (Edge e : graph[src]) {
+      if (!vis[e.v]) allSolution(
+        e.v,
+        dest,
+        vis,
+        p,
+        psf + src,
+        wsf + e.w,
+        givenWeight,
+        k
+      );
+    }
+    vis[src] = false;
+  }
+
+  public static void dfs(int src, boolean[] vis) { //src is vertex
+    vis[src] = true;
+    for (Edge e : graph[src]) {
+      if (!vis[e.v]) dfs(e.v, vis);
+    }
+  }
+
+  public static void gcc() {
+    boolean[] vis = new boolean[N];
+    int components = 0;
+    for (int i = 0; i < N; i++) { //har vertex se dfs call krege[has path wala] if it is unvisited
+      if (!vis[i]) {
+        components++;
+        dfs(i, vis);
+      }
+    }
   }
 
   public static void main(String[] args) {
@@ -86,17 +237,33 @@ public class l001_Basic {
     }
 
     addEdge(0, 1, 10);
-    addEdge(0, 3, 10);
+    addEdge(0, 3, 40);
     addEdge(1, 2, 10);
-    addEdge(2, 3, 40);
+    addEdge(2, 3, 10);
     addEdge(3, 4, 2);
-    addEdge(4, 5, 2);
+    addEdge(4, 5, 3);
     addEdge(4, 6, 8);
     addEdge(5, 6, 3);
+    addEdge(2, 5, 5);
 
     boolean[] vis = new boolean[N];
-    System.out.print(hasPath(0, 6, vis));
-
-    display();
+    // System.out.print(allPaths(0, 6, vis, ""));
+    // printPreOrder(0, vis, "", 0);
+    // postOrder(0, vis, "", 0);
+    Pair p = new Pair(); //kuch paas nahi kra
+    allSolution(0, 6, vis, p, "", 0, 30, 4);
+    System.out.println(
+      "smallest Path= " + p.smallestPath + "@" + p.smallestWeight
+    );
+    System.out.println(
+      "largest Path= " + p.largestPath + "@" + p.largestWeight
+    );
+    System.out.println("Ceil  of 30 = " + p.ceil);
+    System.out.println("Floor of 30 = " + p.floor);
+    System.out.println(
+      "Kth Largest Path = " + pq.peek().psf + "@" + pq.peek().wsf
+    ); //pq sbse kth largest to peek pr hoga , pq is min type
+    //nikala nhi hai bss direct same krre hai
+    // display();
   }
 }
